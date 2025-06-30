ee819877306b51773801f542c45914e824a61dbe
thanhbinh12ii <nptbinh17092004@gmail.com>
2025-07-01 00:18:41 +0700

    fix deploy

--- metro-app-fe/vite.config.ts
+++ metro-app-fe/vite.config.ts
@@ -1,24 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:4003",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: "http://localhost:4003",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      "process.env": env,
+    },
+  };
 });
